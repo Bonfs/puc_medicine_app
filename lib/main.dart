414f9609929e0d2ceb99a653d32843eb536b320f
bonfs <matheusbonfim05@gmail.com>
2024-11-27 18:48:08 -0300

    add drawer

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,10 @@ import 'package:lembrete_remedio/ui/core/app_view_model.dart';
 import 'package:lembrete_remedio/ui/screens/home_screen/home_screen.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(
-  ChangeNotifierProvider(
-    create: (context) => AppViewModel(),
-    child: const MyApp(),
-  )
-);
+void main() => runApp(ChangeNotifierProvider(
+      create: (context) => AppViewModel(),
+      child: const MyApp(),
+    ));
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -29,16 +27,47 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.orange,
           title: const Text('App de remédio'),
         ),
-        body: const HomeScreen(),
-        floatingActionButton: Consumer<AppViewModel>(
-          builder: (context, state, child) {
-            return FloatingActionButton(
-              onPressed: () => state.startIntent(SaveMedicineIntent(Medicine(name: 'tylenol'))),
-              tooltip: 'adicionar novo remédio',
-              child: const Icon(Icons.add),
-            );
-          }
+        drawer: Drawer(
+          // Add a ListView to the drawer. This ensures the user can scroll
+          // through the options in the drawer if there isn't enough vertical
+          // space to fit everything.
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text('Drawer Header'),
+              ),
+              ListTile(
+                title: const Text('Item 1'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+              ListTile(
+                title: const Text('Item 2'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+            ],
+          ),
         ),
+        body: const HomeScreen(),
+        floatingActionButton:
+            Consumer<AppViewModel>(builder: (context, state, child) {
+          return FloatingActionButton(
+            onPressed: () => state
+                .startIntent(SaveMedicineIntent(Medicine(name: 'tylenol'))),
+            tooltip: 'adicionar novo remédio',
+            child: const Icon(Icons.add),
+          );
+        }),
       ),
     );
   }
